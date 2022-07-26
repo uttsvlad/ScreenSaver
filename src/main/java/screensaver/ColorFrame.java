206ff6ed216eaf7fd7a3e1d6f56d06a0c73e3d5e
uttsvlad <uttsvlad@gmail.com>
2022-07-26 16:29:58 +0300

    Initial commit

---
+++ src/main/java/screensaver/ColorFrame.java
@@ -0,0 +1,29 @@
+package screensaver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+@Component
+public abstract class ColorFrame extends JFrame {
+    @Autowired
+    private Color color;
+
+    //TODO make new scope
+    public ColorFrame() {
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace() {
+        Random random = new Random();
+        setLocation(random.nextInt(1800), random.nextInt(900));
+        getContentPane().setBackground(getColor());
+    }
+
+    protected abstract Color getColor();
+}
---
+++ src/main/java/screensaver/Config.java
@@ -0,0 +1,35 @@
+package screensaver;
+
+import org.springframework.context.annotation.*;
+
+import java.awt.*;
+import java.util.Random;
+
+@Configuration
+@ComponentScan(basePackages = "screensaver")
+public class Config {
+    @Bean
+    @Scope("prototype")
+    public Color color() {
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+
+    @Bean
+    public ColorFrame colorFrame(){
+        return new ColorFrame() {
+            @Override
+            protected Color getColor() {
+                return color();
+            }
+        };
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        while (true) {
+            context.getBean(ColorFrame.class).showOnRandomPlace();
+            Thread.sleep(500);
+        }
+    }
+}
